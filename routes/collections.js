a4462f964a24863dc06e290ccdbd68222fa7936f
ddexta <34339529+ddexta@users.noreply.github.com>
2018-12-21 17:59:23 +0200

    added single item deletion by id

--- routes/collections.js
+++ routes/collections.js
@@ -4,6 +4,8 @@ const Collection = require('../modules/Collection');
 const passport = require('passport');
 const validateAdditionInput = require('../validation/addition');
 const moment = require('moment');
+const idGenerator = require('../generators/idGenerator');
+
 //GET COLLECTION ../collection/
 router.get(
   '/',
@@ -27,7 +29,8 @@ router.post(
       const { id } = req.user;
       const date = moment().format('MMM Do YYYY, h:mm:ss a');
       req.body.amount = parseFloat(req.body.amount).toFixed(2);
-      const input = { ...req.body, date };
+      const _id = idGenerator();
+      const input = { ...req.body, date, _id };
       const { errors, isValid } = validateAdditionInput(req.body);
       // Check Validation
       if (!isValid) {
@@ -46,6 +49,27 @@ router.post(
   }
 );
 //DELETE SINGLE
+router.delete(
+  '/delete/:id',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res) => {
+    try {
+      const { id } = req.user;
+      const deletionId = req.params.id;
+      req.body.amount = parseFloat(req.body.amount).toFixed(2);
+
+      const updatedCollection = await Collection.findByIdAndUpdate(
+        id,
+        { $pull: { data: { _id: deletionId } } },
+        { new: true }
+      );
+      await updatedCollection.save();
+      res.json({ sum: updatedCollection.sum, data: updatedCollection.data });
+    } catch (error) {
+      res.json({ error });
+    }
+  }
+);
 //DELETE ALL DATA
 //DELETE COLLECTION
 module.exports = router;
