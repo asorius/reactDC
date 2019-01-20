08ec636d7b3573172367258c13635a88a6bbda67
ddexta <18assdd@gmail.com>
2019-01-20 15:23:36 +0000

    ready for deployment

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const login = require('./routes/login');
 const create = require('./routes/create');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 const app = express();
 //MIDDLEWARE
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,6 +26,15 @@ mongoose
 app.use('/login', login);
 app.use('/create', create);
 app.use('/collections', collections);
+
+//server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server running on port ${port}`));
